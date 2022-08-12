e306fe99ed251921ecba7aaf39af24079c74417e
Semfry <shaunfriend8@gmail.com>
2022-08-12 03:21:57 +0100

    Thur11082022

--- PlymouthPull.py
+++ PlymouthPull.py
@@ -1,4 +1,4 @@
-import requests, bs4, json, pandas
+import requests, bs4, json, pandas, csv
 from reportlab.pdfgen import canvas
 from reportlab.lib import colors
 from reportlab.lib.units import cm
@@ -12,70 +12,95 @@ from reportlab.platypus import (
     Image,
 )
 
-# Get data from Plymouth Data site and open as JSON
+# Choose whether to Get Json data from Plymouth Data site
 
-plyDataArrive = requests.get(
-    "https://plymouth.thedata.place/dataset/479dd1bd-3b15-4640-a259-32b2c440445b/resource/cda4367c-090f-42e7-80ae-827b19e3fc07/download/peoplecount-arrival.geojson"
-)
-plyDataArrive.raise_for_status
-plyData = open("parkarrivedata.json", "wb")
-for chunk in plyDataArrive.iter_content(100000):
-    plyData.write(chunk)
+print("Gather Park data from Plymouth site?")
+parkDownChoice = input().lower()
+x = "true"
+while x == "true":
+    if parkDownChoice == "yes":
+        plyDataArrive = requests.get(
+            "https://plymouth.thedata.place/dataset/479dd1bd-3b15-4640-a259-32b2c440445b/resource/cda4367c-090f-42e7-80ae-827b19e3fc07/download/peoplecount-arrival.geojson"
+        )
+        plyDataArrive.raise_for_status
+        plyData = open("parkarrivedata.json", "wb")
+        for chunk in plyDataArrive.iter_content(100000):
+            plyData.write(chunk)
 
-plyData.close()
+        plyData.close()
+        break
+    elif parkDownChoice == "no":
+        print("continuing to next section...")
+        break
+    else:
+        print("Please enter yes or no")
+        parkDownChoice = input().lower()
 
 # ----------------------------------------------------
-# Make PDF from data (ID, Age, Activity)
-# PDF SETTINGS BELOW
+# Base PDF Settings for Reportlab
+
+elements = []
+
+plyDoc = SimpleDocTemplate(
+    "Plydata.pdf",
+    pagesize=A4,
+)
+
+styleSheet = getSampleStyleSheet()
+
+# ----------------------------------------------------
+# Make PDF from data
 
 # with open("parkarrivedata.json", "r") as parkJson:
 #     parkData = json.load(parkJson)
+# summary = [["Age", "Activity"]]
 # for i in range(len(parkData["features"])):
-#     # print(parkData["features"][i]["properties"]["Age"])
 #     agesp = parkData["features"][i]["properties"]["Age"]
+
+#     P0 = Paragraph(
+#         agesp,
+#         styleSheet["BodyText"],
+#     )
+
 #     activityp = parkData["features"][i]["properties"]["Activity"]
 
-# elements = []
+#     P1 = Paragraph(
+#         activityp,
+#         styleSheet["BodyText"],
+#     )
+#     summary.append([P0, P1])
 
-# plyDoc = SimpleDocTemplate(
-#     "Plydata.pdf",
-#     pagesize=A4,
-# )
-#=======
-#elements = []
+with open("parkarrivepandas.csv", "r") as parkJson:
+    parkData = csv.reader(parkJson)
+    summary = [["Age", "Activity"]]
+    for i in parkData:
+        agesp = parkData["Age"]
 
-# styleSheet = getSampleStyleSheet()
+        P0 = Paragraph(
+            agesp,
+            styleSheet["BodyText"],
+        )
 
-# P0 = Paragraph(
-#     agesp,
-#     styleSheet["BodyText"],
-# )
+        activityp = parkData["Activity"]
 
-# P1 = Paragraph(
-#     activityp,
-#     styleSheet["BodyText"],
-# )
+        P1 = Paragraph(
+            activityp,
+            styleSheet["BodyText"],
+        )
+        summary.append([P0, P1])
 
-# plyParkTable = [
-#     ["Age", "Activity"],
-#     [P0, P1],
-#     # ["20", "21"],
-#     # ["30", "31"],
-# ]
+    plyP = Table(
+        summary,
+        style=[
+            ("GRID", (0, 0), (-1, -1), 1, colors.black),
+            ("BOX", (0, 0), (-1, -1), 2, colors.black),
+        ],
+    )
 
-# plyP = Table(
-#     plyParkTable,
-#     style=[
-#         ("GRID", (0, 0), (-1, -1), 1, colors.black),
-#         ("BOX", (0, 0), (-1, -1), 2, colors.black),
-#     ],
-# )
-# # plyP._argW[3] = 1.5 * inch
+elements.append(plyP)
 
-# elements.append(plyP)
+plyDoc.build(elements)
 
-# plyDoc.build(elements)
+# Close open files
 
-# # Close open files
-
-# parkJson.close()
+parkJson.close()
